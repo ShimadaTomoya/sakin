45e5aca368f74a9add0001cbfec63414537b0f3d
ShimadaTomoya <tomoya.shimada@naviplus.co.jp>
2022-02-19 03:15:35 +0900

    invert_indexモジュール作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+pub mod invert_index {
+    pub struct Token {
+        id: u64,
+        body: String
+    }
+
+    pub struct Document {
+        id: u64,
+        body: String
+    }
+
+    pub fn build_token(id: u64, body: String) -> Token {
+        Token {
+            id,
+            body
+        }
+    }
+
+    pub fn build_document(id: u64, body: String) -> Document {
+        Document {
+            id,
+            body
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _token = invert_index::build_token(0, "body".to_string());
+    let _document = invert_index::build_document(0, "body".to_string());
 }
